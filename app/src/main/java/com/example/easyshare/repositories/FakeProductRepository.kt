fecaeab0802f60e9665fdc0a78dca067a4ad1445
Mazene <mmazenezerguine@gmail.com>
2023-12-17 16:49:56 +0100

    conflic: fixing conflicts

--- app/src/main/java/com/example/easyshare/repositories/FakeProductRepository.kt
+++ app/src/main/java/com/example/easyshare/repositories/FakeProductRepository.kt
@@ -7,12 +7,11 @@ import com.example.easyshare.services.ApiService
 import com.example.easyshare.models.ProductData
 import io.reactivex.rxjava3.core.Flowable
 
-class FakeProductRepository(private val fakeProductData: FakeProductService) {
+
+class FakeProductRepository(private val fakeProductData: ApiService) {
     private val imageUrl =
         "https://encrypted-tbn0.gstatic.com/images?" +
-            "q=tbn:ANd9GcSY0jbtT-z4XUqN0R9yhdwhiA-h_pPRnSbnCFAfjnKITa1GxgXoHN_NMoEsM9jC72Omek0&usqp=CAU"
-class FakeProductRepository(private val fakeProductData: ApiService) {
-
+          "q=tbn:ANd9GcSY0jbtT-z4XUqN0R9yhdwhiA-h_pPRnSbnCFAfjnKITa1GxgXoHN_NMoEsM9jC72Omek0&usqp=CAU"
     fun getFakeProducts(): Flowable<List<ProductData>> {
         val dummyData =
             listOf(
@@ -111,3 +110,5 @@ class FakeProductRepository(private val fakeProductData: ApiService) {
         return Flowable.just(dummyData)
     }
 }
+
+
