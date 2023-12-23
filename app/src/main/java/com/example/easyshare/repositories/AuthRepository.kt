888655c2aa747cf6f980e650cb193777c04797ad
Mazene <mmazenezerguine@gmail.com>
2023-12-23 20:07:48 +0100

    feat: adding signup request and response models and repository

--- app/src/main/java/com/example/easyshare/repositories/AuthRepository.kt
+++ app/src/main/java/com/example/easyshare/repositories/AuthRepository.kt
@@ -2,11 +2,17 @@ package com.example.easyshare.repositories
 
 import com.example.easyshare.models.LoginRequest
 import com.example.easyshare.models.LoginResponse
+import com.example.easyshare.models.SignupRequest
+import com.example.easyshare.models.SignupResponse
 import com.example.easyshare.services.ApiService
 import io.reactivex.rxjava3.core.Flowable
 
 class AuthRepository(private val apiService: ApiService) {
     fun login(payload: LoginRequest): Flowable<LoginResponse> {
         return apiService.userLogin(payload)
+    }
+
+    fun signup(payload: SignupRequest): Flowable<SignupResponse> {
+        return this.apiService.signup(payload)
     }
 }
