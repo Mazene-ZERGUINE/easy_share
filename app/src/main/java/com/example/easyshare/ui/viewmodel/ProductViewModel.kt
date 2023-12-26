5039b1b1d205cd89e6d838f12243c04776353cfb
LoicZHU <zhu.loic@gmail.com>
2023-12-26 20:25:01 +0100

    feat(homeFragment): add un/star post

--- app/src/main/java/com/example/easyshare/ui/viewmodel/ProductViewModel.kt
+++ app/src/main/java/com/example/easyshare/ui/viewmodel/ProductViewModel.kt
@@ -6,14 +6,18 @@ import androidx.lifecycle.ViewModel
 import com.example.easyshare.models.Data
 import com.example.easyshare.repositories.ProductsRepository
 import com.example.easyshare.repositories.UserRepository
+import io.reactivex.rxjava3.core.Observable
 import io.reactivex.rxjava3.disposables.CompositeDisposable
 import io.reactivex.rxjava3.kotlin.addTo
 import io.reactivex.rxjava3.schedulers.Schedulers
 import io.reactivex.rxjava3.subjects.BehaviorSubject
 
-class ProductViewModel(private val productRepository: ProductsRepository, private val userRepository: UserRepository) : ViewModel() {
+class ProductViewModel(
+    private val productRepository: ProductsRepository,
+    private val userRepository: UserRepository
+) : ViewModel() {
     private val disposBag = CompositeDisposable()
-
+    private val starPosts = mutableMapOf<String, Boolean>() // <postId, starred>
     private val productData: BehaviorSubject<List<Data>> = BehaviorSubject.createDefault(listOf())
 
     val isLoading: MutableLiveData<Boolean> = MutableLiveData(true)
@@ -24,12 +28,19 @@ class ProductViewModel(private val productRepository: ProductsRepository, privat
         getCompleteProductData()
     }
 
+    fun isPostStarred(key: String): Boolean? {
+        return starPosts[key]
+    }
+
+    fun reloadPosts() {
+        this.getProducts()
+    }
+
     fun starPost(id: Int) {
         this.userRepository.starPost(id)
             .observeOn(Schedulers.io())
             .subscribe(
                 {
-                    this.getProduct()
                 },
                 { e ->
                     Log.d("starPost", "Error while star post")
@@ -37,14 +48,55 @@ class ProductViewModel(private val productRepository: ProductsRepository, privat
             ).addTo(disposBag)
     }
 
-    private fun getProduct() {
+    fun unstarPost(id: Int) {
+        this.userRepository.unstarPost(id)
+            .observeOn(Schedulers.io())
+            .subscribe(
+                {
+                },
+                { e ->
+                    Log.d("starPost", "Error while star post")
+                }
+            ).addTo(disposBag)
+    }
+
+    private fun getIsPostStarred(id: Int): Observable<Boolean> {
+        return this.userRepository.isStarredPost(id)
+            .observeOn(Schedulers.io())
+            .map {
+                this.starPosts[id.toString()] = it.data.starred
+                return@map it.data.starred
+            }
+            .onErrorReturn { _ ->
+                Log.d("getIsStarredPost", "Error while getting getIsStarredPost")
+                false
+            }
+    }
+
+    private fun getProducts() {
         productRepository.getProducts()
             .observeOn(Schedulers.io())
             .doOnTerminate { this.isLoading.postValue(false) }
+            .flatMapObservable { response ->
+                this.productData.onNext(response.data)
+
+                Observable.fromIterable(response.data)
+                    .flatMap { data ->
+                        getIsPostStarred(data.publicationId).map { isStarred ->
+                            Pair(
+                                data,
+                                isStarred
+                            )
+                        }
+                    }
+                    .toList()
+                    .toObservable()
+            }
             .subscribe(
-                {
-                    this.productData.onNext(it.data)
-                    Log.d("commentsSize", it.data[0].comments.count().toString())
+                { pairs ->
+                    pairs.map { (data, isStarred) ->
+                        starPosts.put(data.publicationId.toString(), isStarred)
+                    }
                 },
                 { e ->
                     Log.e("getProducts", "Error while getting products data", e)
@@ -53,7 +105,7 @@ class ProductViewModel(private val productRepository: ProductsRepository, privat
     }
 
     private fun getCompleteProductData() {
-        this.getProduct()
+        this.getProducts()
 
         this.productData
             .observeOn(Schedulers.io())
