64c291b0d276c23a4953acfca1a74ef420037e78
Mazene <mmazenezerguine@gmail.com>
2023-12-26 21:18:32 +0100

    feat: profile page setting data to inputs

--- app/src/main/java/com/example/easyshare/ui/fragments/AccountFragment.kt
+++ app/src/main/java/com/example/easyshare/ui/fragments/AccountFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.easyshare.R
 import com.example.easyshare.ui.viewmodel.AccountViewModel
@@ -14,11 +17,17 @@ class AccountFragment : Fragment() {
     private val accountViewModel: AccountViewModel by viewModel()
 
     private val pseudonyme: String = TokenManager.getPseudonymeFromToken()
+    private lateinit var userNameTv: TextView
+    private lateinit var emailTv: TextView
+    private lateinit var nameInput: EditText
+    private lateinit var lastNameInput: EditText
+    private lateinit var emailInput: EditText
+    private lateinit var userNameInput: EditText
+    private lateinit var passwordInput: EditText
+    private lateinit var editButton: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        setUserData()
     }
 
     override fun onCreateView(
@@ -26,20 +35,36 @@ class AccountFragment : Fragment() {
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_account, container, false)
+        val rootView = inflater.inflate(R.layout.fragment_account, container, false)
+        userNameTv = rootView.findViewById(R.id.userNameTextView)
+        emailTv = rootView.findViewById(R.id.userEmailTextView)
+        nameInput = rootView.findViewById(R.id.editTextName)
+        lastNameInput = rootView.findViewById(R.id.editTextLastName)
+        emailInput = rootView.findViewById(R.id.editTextEmail)
+        userNameInput = rootView.findViewById(R.id.editTextUserName)
+        passwordInput = rootView.findViewById(R.id.editTextPassword)
+        editButton = rootView.findViewById(R.id.editButton)
+
+        setUserData()
+
+        return rootView
     }
 
     private fun setUserData() {
         println(pseudonyme)
         accountViewModel.getUserByPseudonyme(pseudonyme)
 
-        accountViewModel.getUserDataResult.observe(this@AccountFragment) { response ->
-            println(response)
+        accountViewModel.getUserDataResult.observe(viewLifecycleOwner) { response ->
+            userNameTv.text = response.pseudonyme
+            emailTv.text = response.email
+
+            userNameInput.setText(response.pseudonyme)
+            nameInput.setText(response.nom)
+            lastNameInput.setText(response.prenom)
+            emailInput.setText(response.email)
         }
 
-        accountViewModel.getUserError.observe(this@AccountFragment) { err ->
-            println(err)
-            err.printStackTrace()
+        accountViewModel.getUserError.observe(viewLifecycleOwner) { err ->
         }
     }
 }
